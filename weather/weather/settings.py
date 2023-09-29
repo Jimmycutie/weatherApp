816b61acdab9e158ec8b4088be9ca69fd890e01b
Mihir <82092317+Jimmycutie@users.noreply.github.com>
2023-09-30 00:25:51 +0530

    bugfix

--- weather/weather/settings.py
+++ weather/weather/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0k0_6fst2y(t(@1xdjh=ttxglfrp%@=x2+@-8rmwqy^^nah_pt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
